a1908e77d61b354a146378b0a494ee558c3c8a8a
Alonso <user@Users-MacBook-Pro.local>
2026-05-05 19:03:56 +1000

    revert: back to 10-band EQ table, keep full-range correction

--- src/main.js
+++ src/main.js
@@ -960,14 +960,7 @@ function populateEQTable(visData, gains) {
     return { gain: gains[gains.length - 1], response: visData[visData.length - 1].y };
   }
 
-  // Extended frequency bands for display (includes out-of-range for full picture)
-  const DISPLAY_BANDS = [
-    20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160,
-    200, 250, 315, 400, 500, 630, 800, 1000, 1250, 1600,
-    2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500, 16000, 20000
-  ];
-
-  for (const freq of DISPLAY_BANDS) {
+  for (const freq of EQMAC_BANDS) {
     const { gain, response } = getGainAtFreq(freq);
     const gainNum = Number(gain);
     const respNum = Number(response);
